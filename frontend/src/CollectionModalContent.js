eb3bd05223134bc864782236cbe5474420b46e38
Robert Ku <robertku79@gmail.com>
2022-03-16 01:00:35 -0400

    Add details to collection

--- frontend/src/CollectionModalContent.js
+++ frontend/src/CollectionModalContent.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { QuestionMarkCircleIcon } from '@heroicons/react/solid';
 
 const CollectionModalContent = ({ collection }) => {
+  const [hoveredId, setHoveredId] = useState(null);
+  const [names, setNames] = useState(null);
+
+  useEffect(() => {
+    getNames();
+  }, []);
+
+  async function getNames() {
+    const res = await fetchNames();
+    const resJSON = await res.json();
+
+    const names = {};
+    resJSON.results.forEach((result) => {
+      const splitRes = result.url.split('/');
+      const key = splitRes[splitRes.length - 2];
+      names[key] = result.name;
+    });
+
+    setNames(names);
+  }
+
+  function fetchNames() {
+    const request = new Request('https://pokeapi.co/api/v2/pokemon?limit=898', {
+      method: 'GET'
+    });
+
+    return fetch(request);
+  }
+
   const collectionMap = {};
 
   collection.forEach((imageUrl) => {
@@ -16,7 +45,27 @@ const CollectionModalContent = ({ collection }) => {
 
   for (let id = 1; id < 899; id++) {
     if (ids.includes(id)) {
-      items.push(<img key={id} src={collectionMap[id]} className="h-16 w-16" />);
+      items.push(
+        <div key={id} className="relative">
+          <img
+            onClick={() => {
+              if (hoveredId === id) {
+                setHoveredId(null);
+              } else {
+                setHoveredId(id);
+              }
+            }}
+            src={collectionMap[id]}
+            className="h-16 w-16"
+          />
+          {hoveredId === id && (
+            <div className="flex-col text-xs rounded-full flex items-center justify-center capitalize bg-black opacity-70 text-white h-full w-full absolute top-0 left-0 pointer-events-none">
+              <div>#{id}</div>
+              <div>{names[id]}</div>
+            </div>
+          )}
+        </div>
+      );
     } else {
       items.push(<QuestionMarkCircleIcon key={id} className="h-16 w-16" />);
     }
